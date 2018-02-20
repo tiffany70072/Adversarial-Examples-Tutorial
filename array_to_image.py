466da7ed57ec5f04c0146db9d964df9acfe5daef
tiffany70072 <b03202017@ntu.edu.tw>
2018-02-20 18:21:51 +0800

    Update array_to_image.py

--- array_to_image.py
+++ array_to_image.py
@@ -2,17 +2,14 @@ import numpy as np
 import PIL.Image as Image
 import matplotlib.pyplot as pyplot
 
-def array_to_image(arr, image_filename, channel = 1, size = 28, normalized = True):
-    '''
-    For mnist, channel = 1, size = 28
-    '''
+def array_to_image(arr, image_filename, channels = 1, size = 28, normalized = True):
+    # For mnist, channels = 1, size = 28
+    # For cifar-10, channels = 3, size = 32
 
     if normalized == True: n = 255.0
     else: n = 1.0
     rows = arr.shape[0]
-    print arr.shape
-    arr = arr.reshape(rows, channel, size, size)
-
+    arr = arr.reshape(rows, channels, size, size)
 
     for index in range(rows):
         a = arr[index]
